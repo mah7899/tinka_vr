fc39fa9e73dace7b205335daa4c8a1d17c4fb6c5
mah7899 <71802561+mah7899@users.noreply.github.com>
2023-09-22 10:37:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
-// server.js
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-app.use(cors());
-
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Parse JSON body
 
 app.post('/api/chat', async (req, res) => {
-  try {
-    const question = req.body.question;
-    const response = await axios.post(
-      'https://api.openai.com/v1/chat/completions',
-      { messages: [{ role: 'user', content: question }], model: 'gpt-3.5-turbo' },
-      { headers: { Authorization: `Bearer ${process.env.OPENAI_API_KEY}` } }
-    );
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
+    try {
+        const { question } = req.body;
+        const openaiUrl = "https://api.openai.com/v1/chat/completions";
+        
+        const openaiResponse = await axios.post(openaiUrl, {
+            messages: [{ role: "user", content: question }],
+            temperature: 0.5,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            model: "gpt-3.5-turbo"
+        }, {
+            headers: {
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        const answer = openaiResponse.data.choices[0].message.content;
+        res.json({ answer });
+        
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
