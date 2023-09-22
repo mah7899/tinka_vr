c10905f5fb93dd1304460acc8e8f67482bc4c1bb
mah7899 <71802561+mah7899@users.noreply.github.com>
2023-09-22 10:32:48 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+// server.js
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/api/chat', async (req, res) => {
+  try {
+    const question = req.body.question;
+    const response = await axios.post(
+      'https://api.openai.com/v1/chat/completions',
+      { messages: [{ role: 'user', content: question }], model: 'gpt-3.5-turbo' },
+      { headers: { Authorization: `Bearer ${process.env.OPENAI_API_KEY}` } }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
